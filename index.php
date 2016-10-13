84aac45826f8e47eb25949a4b5f0cc12fb921f82
Aren <osacar.lee@gmail.com>
2016-10-13 16:50:58 +0800

    空文件

--- index.php
+++ index.php
@@ -1,9 +1,2 @@
 <?php
-use Aren\Core\Core;
-
-define('TIME_START', microtime(true));
-define('MEMORY_START', memory_get_usage());
-	
-include "./Aren/Core/Core.class.php";
-
-Core::bootstrap();
+exit('deny');
